fbdf5eed966246c21cec06352baea7d58da4e241
Tiago Silva de Assis <tiaguiassis@gmail.com>
2024-09-30 07:35:21 -0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
 const currentDate = new Date()
 const addEntregador = document.getElementById('addEntregador')
 
-let weekDay = currentDate.getDay()
-let day = currentDate.getDate()
-let month = currentDate.getMonth() + 1
-let deliveryPerson
+let formattedReport = document.querySelector('#formatted-report')
 
 document.addEventListener('DOMContentLoaded', ()=>{
     const legend = document.querySelector('legend')
-    let divDelivery = document.querySelectorAll('.div-entregadores')
+    let weekDay = currentDate.getDay()
+    let day = currentDate.getDate()
+    let month = currentDate.getMonth() + 1
+    let divDelivery = document.querySelectorAll('.delivery-person-container')
+    
     if (currentDate.getHours() >= 5 && currentDate.getHours() <= 17) { // dia
         legend.innerText = 'Almoço ' + day + '/' + month
+        formattedReport.innerHTML = '*Almoço ' + day + '/' + month + '*<br>'
         divDelivery[0].style.display = 'none'; // caso seja de dia, os entregadores da noite não aparecem
         divDelivery[1].style.display = 'none';
         divDelivery[2].style.display = 'none';
@@ -19,66 +21,58 @@ document.addEventListener('DOMContentLoaded', ()=>{
         divDelivery[3].style.display = 'none'; // desaparece com o entregador do dia
         switch (weekDay) { // define qual entregador estara de folga
             case 1:
-                deliveryPerson = document.getElementById('div-entregador5')
-                deliveryPerson.style.display = 'none'
+                divDelivery[4].style.display = 'none';
                 break;
         
             case 2:
-                deliveryPerson = document.getElementById('div-entregador2')
-                deliveryPerson.style.display = 'none'
+                divDelivery[1].style.display = 'none';
                 break;
         
             case 3:
-                deliveryPerson = document.getElementById('div-entregador1')
-                deliveryPerson.style.display = 'none'
+                divDelivery[0].style.display = 'none';
                 break;
         
             case 4:
-                deliveryPerson = document.getElementById('div-entregador3')
-                deliveryPerson.style.display = 'none'
+                divDelivery[2].style.display = 'none';
                 break;
         }
         if(currentDate.getHours() >= 0 && currentDate.getHours() <= 4){
             day--
             legend.innerText = 'Noite ' + day + '/' + month
+            formattedReport.innerHTML = '*Noite ' + day + '/' + month + '*<br>'
         }else{
             legend.innerText = 'Noite ' + day + '/' + month
+            formattedReport.innerHTML = '*Noite ' + day + '/' + month + '*<br>'
         }
     }
     
 })
 
-function ClearInput(idInput) { // limpa o input para selecionar outro entregador
-    let input = document.getElementById(idInput)
-    input.value = ''
-    setTimeout(() => {
-        input.focus();
-    }, 0)
-}
-
-addEntregador.addEventListener('click', () => {
-    const container = document.getElementById('container-entregadores')
+function AddDeliveryPerson() {
+    const container = document.getElementById('delivery-persons-container')
     const newHr = document.createElement('hr')
-    let newDiv, newFieldset, newLabel, newInput
-    let button = document.getElementById('addEntregador')
+    const button = document.getElementById('add-delivery-person')
+    let newDiv, newFieldset, newLabel, newInput, deliveryPersonId
 
     newDiv = document.createElement('div')
-    newDiv.id = 'div-' + Date.now()
-    newDiv.className = 'div-entregadores'
+    newDiv.className = 'delivery-person-container'
 
     for (let index = 0; index < 4; index++) {
 
         switch (index) {
             case 0:
+                deliveryPersonId = 'delivery-person-name-' + Date.now() + index
                 newLabel = document.createElement('label')
                 newLabel.textContent = 'Nome'
-                newLabel.setAttribute('for', Date.now() + index)
+                newLabel.setAttribute('for', deliveryPersonId)
 
                 newInput = document.createElement('input')
-                newInput.id = 'input-' + Date.now() + index
+                newInput.id = deliveryPersonId
                 newInput.type = 'text'
-                newInput.setAttribute('list', 'datalist-entregadores')
-                newInput.onfocus = "ClearInput('entregador1')"
+                newInput.setAttribute('list', 'datalist-delivery-person')
+                newInput.onfocus = function(){
+                    ClearInput(deliveryPersonId)
+                }
                 newInput.value = ''
                 newInput.placeholder = 'Entregador'
             break;
@@ -86,10 +80,10 @@ addEntregador.addEventListener('click', () => {
             case 1:
                 newLabel = document.createElement('label')
                 newLabel.textContent = 'Entregas'
-                newLabel.setAttribute('for', Date.now() + index)
+                newLabel.setAttribute('for', 'deliveries-' + Date.now() + index)
 
                 newInput = document.createElement('input')
-                newInput.id = 'input-' + Date.now() + index
+                newInput.id = 'deliveries-' + Date.now() + index
                 newInput.type = 'number'
                 newInput.placeholder = 'Quantindade de Entregas'
             break;
@@ -97,10 +91,10 @@ addEntregador.addEventListener('click', () => {
             case 2:
                 newLabel = document.createElement('label')
                 newLabel.textContent = 'Extra'
-                newLabel.setAttribute('for', Date.now() + index)
+                newLabel.setAttribute('for', 'extra-' + Date.now() + index)
 
                 newInput = document.createElement('input')
-                newInput.id = 'input-' + Date.now() + index
+                newInput.id = 'extra-' + Date.now() + index
                 newInput.type = 'number'
                 newInput.placeholder = 'Quantindade de Extra'
             break;
@@ -108,10 +102,10 @@ addEntregador.addEventListener('click', () => {
             case 3:
                 newLabel = document.createElement('label')
                 newLabel.textContent = 'Consumo'
-                newLabel.setAttribute('for', Date.now() + index)
+                newLabel.setAttribute('for', 'consumption-' + Date.now() + index)
 
                 newInput = document.createElement('input')
-                newInput.id = 'input-' + Date.now() + index
+                newInput.id = 'consumption-' + Date.now() + index
                 newInput.type = 'text'
                 newInput.placeholder = 'Valor de Consumo'
             break;
@@ -131,30 +125,150 @@ addEntregador.addEventListener('click', () => {
 
     newDiv.appendChild(newHr)
     container.insertBefore(newDiv, button)
+}
+
+function ClearInput(idInput) { // limpa o input para selecionar outro entregador
+    let input = document.getElementById(idInput)
+    input.value = ''
+    setTimeout(() => {
+        input.focus();
+    }, 0)
+}
+
+document.querySelector('.copy-button').addEventListener('click', function() {
+    const relatorio = document.getElementById('formatted-report');
+    const textoRelatorio = relatorio.innerText || relatorio.textContent;
+
+    navigator.clipboard.writeText(textoRelatorio).then(() => {
+        showPopup();  // Função para exibir o popup
+    }).catch((err) => {
+        alert('Erro ao copiar: ' + err);
+    });
 });
 
 
-/*
-document.getElementById('copiarRelatorio').addEventListener('click', function() {
-            const relatorio = document.getElementById('relatorioFormatado');
-            const range = document.createRange();
-            range.selectNode(relatorio);
-            window.getSelection().removeAllRanges(); // Limpa a seleção atual
-            window.getSelection().addRange(range); // Seleciona o conteúdo
+function showPopup() { // Função para mostrar o popup e escondê-lo depois de 3 segundos
+    const popup = document.getElementById('popup');
+    popup.classList.add('show');
+    
+    setTimeout(() => {
+        popup.classList.remove('show');
+    }, 3000); // O popup desaparece após 3 segundos
+}
 
-            // Tenta copiar o conteúdo
-            try {
-                const sucesso = document.execCommand('copy');
-                if (sucesso) {
-                    alert('Relatório copiado para a área de transferência!');
-                } else {
-                    alert('Falha ao copiar.');
+function UpdateReport(event) {
+    const element = event.target.id
+    let div, p, deliveryPerson
+
+    deliveryPerson = element.match(/\d+/g)[0]
+    let index = document.getElementById(`div-${deliveryPerson}`)
+
+let teste
+    if(index){
+        switch (deliveryPerson) {
+            case '1':
+                
+                break;
+    
+            case '2':
+            
+                break;
+    
+            case '3':
+            
+                break;
+    
+            case '4':
+            
+                break;
+    
+            case '5':
+                if (element.includes('name')) {
                 }
-            } catch (err) {
-                alert('Erro ao copiar: ' + err);
-            }
+                if (element.includes('deliveries')) {
+                    p = document.getElementById(`p-deliveries-${deliveryPerson}`)
+                    let quantityDeliveries = parseFloat(document.getElementById(element).value)
+                    let quantityDeliveriesExtra = parseFloat(document.getElementById('extra-5').value)
+                    let consumption = parseFloat(document.getElementById('consumption-5').value)
 
-            window.getSelection().removeAllRanges(); // Limpa a seleção
-*/
+                    
+
+                    if(quantityDeliveries == NaN){
+                        quantityDeliveries = 0
+                    }
+                    if(quantityDeliveriesExtra == NaN){
+                        quantityDeliveriesExtra = 0
+                    }
+                    if(consumption == NaN){
+                        consumption = 0
+                    }
+                    p.innerHTML = `${quantityDeliveries}<br>${quantityDeliveriesExtra}<br>${consumption}<br>`
+                    console.log(typeof consumption)
+                    //p.innerHTML = 'R$: ' + ((((quantityDeliveries + quantityDeliveriesExtra) * 6) + 10) - consumption)
+                }
+                break;
+    
+            default:
+                break;
+        }
+
+    } else {
+        console.log('Campo não existe')
+    } 
+
+    div = document.createElement('div')
+    div.id = 'div-' + deliveryPerson
+
+    for (let i = 0; i < 5; i++) {
+        p = document.createElement('p')
+        switch (i) {
+            case 0:
+                p.id = 'p-name-' + deliveryPerson
+                break;
+            
+            case 1:
+                p.id = 'p-payment-' + deliveryPerson
+                break;
+
+            case 2:
+                p.id = 'p-deliveries-' + deliveryPerson
+                break;
+
+            case 3:
+                p.id = 'p-extra-' + deliveryPerson
+                break;
+
+            case 4:
+                p.id = 'p-consumption-' + deliveryPerson
+                break;
+
+            default:
+                console.log('Algum erro ocorreu')
+                break;
+
+        }
+        div.appendChild(p)
+    }
+
+    formattedReport.appendChild(div)
+
+    if (element.includes('name')) {
+
+    } else if(element.includes('deliveries')){
+        
+    } else if(element.includes('extra')){
+
+    } else if(element.includes('consumption')){
+
+    } else {
+        formattedReport.innerHTML += 'erro'
+
+    }
+
+    // console.log(element)
+}
+
+// Kaio: R$ 22,60
+// (17 entregas, 1 consumo, 2 extras)
 
 
